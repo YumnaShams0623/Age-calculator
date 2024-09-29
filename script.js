cb99f2911565ec1809754b72f96142f757cd42ea
YumnaShams0623 <yumniijan26471@gmial.com>
2024-09-29 15:49:04 +0500

    changing color

---
+++ script.js
@@ -0,0 +1,30 @@
+function calculateAge() {
+    var birthdateInput = document.getElementById('birthdate').value;
+    if (!birthdateInput) {
+      alert("Please enter a valid date.");
+      return;
+    }
+  
+    var birthdate = new Date(birthdateInput);
+    var today = new Date();
+  
+    var years = today.getFullYear() - birthdate.getFullYear();
+    var months = today.getMonth() - birthdate.getMonth();
+    var days = today.getDate() - birthdate.getDate();
+  
+    if (days < 0) {
+      months--;
+      days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+  
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+  
+    // Update the buttons with the calculated values
+    document.getElementById('years').innerHTML = "Years: " + years;
+    document.getElementById('months').innerHTML = "Months: " + months;
+    document.getElementById('days').innerHTML = "Days: " + days;
+  }
+  
