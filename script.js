bc89afd78014fa64124606b74b9da8eef16c5b1a
YumnaShams0623 <yumniijan26471@gmial.com>
2024-09-29 16:35:05 +0500

    used if statements

--- script.js
+++ script.js
@@ -12,4 +12,13 @@ var today = new Date();
 var years = today.getFullYear() - birthdate.getFullYear();
 var months = today.getMonth() - birthdate.getMonth();
 var days = today.getDate() - birthdate.getDate();
-
+if (days < 0) {
+    months--;
+    days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+  }
+  
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+  
